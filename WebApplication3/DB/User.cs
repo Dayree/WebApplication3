03dbb17435f7bc6623477ece8a4d805b3740671c
Dayree <aysetubakahraman293@gmail.com>
2023-01-05 19:04:37 +0300

    Add project files.

---
+++ WebApplication3/DB/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication3.DB
+{
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
