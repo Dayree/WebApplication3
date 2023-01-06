5abbd21a91b24da16c03b36115dacca2fa5b6e6b
Dayree <aysetubakahraman293@gmail.com>
2023-01-06 14:30:04 +0300

    Products (Uruns) düzenlendi.

--- WebApplication3/DB/AppDbContext.cs
+++ WebApplication3/DB/AppDbContext.cs
@@ -15,13 +15,15 @@ namespace WebApplication3.DB
         }
         public DbSet<User> Users { get; set; }
         public DbSet<Siparis> Siparises { get; set; }
+        public DbSet<WebApplication3.DB.Urun> Urun { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>().ToTable("User");
             modelBuilder.Entity<Siparis>().ToTable("Siparis");
+            modelBuilder.Entity<Urun>().ToTable("Urun");
         }
 
-        public DbSet<WebApplication3.DB.Urun> Urun { get; set; }
+   
     }
 }
