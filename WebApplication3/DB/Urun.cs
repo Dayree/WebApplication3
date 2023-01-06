03de9a72e30c2f3a22d235020f5bf37577c49095
Dayree <aysetubakahraman293@gmail.com>
2023-01-06 11:11:32 +0300

    Urun eklendi.

--- WebApplication3/DB/Urun.cs
+++ WebApplication3/DB/Urun.cs
@@ -7,15 +7,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication3.DB
 {
-    public class User
+    public class Urun
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int Id { get; set; }
-        public string KullaniciAdi { get; set; }
-        public int Sifre { get; set; }
-        public string Email { get; set; }
-        public string Name { get; set; }
-        public string LastName { get; set; }
-        public string Rol { get; set; }
+        public string UrunAdi { get; set; }
+        public string Renk { get; set; }
+        public string Model { get; set; }
+        public int Fiyat { get; set; }
+      
     }
 }
